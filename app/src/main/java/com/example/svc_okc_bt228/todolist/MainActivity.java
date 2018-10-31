1926b207c730942114d76f95185d6b6594ab0de0
Bob Rockers <rockers@okstate.edu>
2018-10-31 18:57:02 -0500

    Initial import into master

---
+++ app/src/main/java/com/example/svc_okc_bt228/todolist/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.svc_okc_bt228.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//This is a comment not meant to do anything
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
