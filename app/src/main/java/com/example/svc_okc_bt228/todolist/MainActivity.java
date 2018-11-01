49077c72dde02407d39927012ba1dca802922dfc
Bob Rockers <rockers@okstate.edu>
2018-10-31 19:09:09 -0500

    Added comment... because you know... code.

--- app/src/main/java/com/example/svc_okc_bt228/todolist/MainActivity.java
+++ app/src/main/java/com/example/svc_okc_bt228/todolist/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.svc_okc_bt228.todolist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// Wait I want it back
 public class MainActivity extends AppCompatActivity {
 
     @Override
